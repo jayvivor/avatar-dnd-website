27a7536b42c1f0192b2146e0ca2116c2676ff3ec
Jadon <jadon1082@gmail.com>
2024-06-21 00:28:27 -0500

    homepage

--- src/avatardnd/urls.py
+++ src/avatardnd/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("home.urls")),
     path("forms/", include("forms.urls")),
     path("admin/", admin.site.urls),
 ]
